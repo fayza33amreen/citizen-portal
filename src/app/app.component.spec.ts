71f78c1980740fe59cef6db4e094f4defb529345
fayza33amreen <38584105+fayza33amreen@users.noreply.github.com>
2018-09-25 17:35:37 +0600

    vs commit

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -13,15 +13,15 @@ describe('AppComponent', () => {
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   }));
-  it(`should have as title 'CitizenPortal'`, async(() => {
+  it(`should have as title 'citizen-portal'`, async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('CitizenPortal');
+    expect(app.title).toEqual('citizen-portal');
   }));
   it('should render title in a h1 tag', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to CitizenPortal!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to citizen-portal!');
   }));
 });
